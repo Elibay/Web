513e606911e017a6c8d94a291d375de017b37cd7
Elibay <elibay98@gmail.com>
2019-03-20 16:02:43 +0600

    some commit

--- Film/src/app/app.module.ts
+++ Film/src/app/app.module.ts
@@ -4,15 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UiModule } from './ui/ui.module';
+import { RouterModule, Routes } from '@angular/router';
+import { DescriptionComponent } from './description/description.component';
+
+const appRoutes: Routes = [
+  { path: '', component: UiModule },
+  { path: 'description', component: DescriptionComponent}
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DescriptionComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    UiModule
+    UiModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
