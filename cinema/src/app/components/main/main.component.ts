3fb03c9a96c09bf7a74a1b0906b25182d402b95e
Kazbek <kazbekborash@gmail.com>
2019-05-14 05:13:27 +0600

    sdasd

--- cinema/src/app/components/main/main.component.ts
+++ cinema/src/app/components/main/main.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Statics} from '../../static/Statics';
+import { Movie } from 'src/app/models/models';
+import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
   selector: 'app-main',
@@ -10,10 +12,52 @@ export class MainComponent implements OnInit {
 
   constructor() { }
 
+  public movies:Array<Movie> = []
+
   ngOnInit() {
     Statics.onInit();
     const index = document.getElementById('index');
     index.classList.add('active');
+    
+    let avenger:Movie = {
+      id: 1,
+      title: "Мстители",
+      country: "США",
+      poster: "",
+      genre: "triller",
+      premiere:  "13123",
+      duration: "1212",
+      age: 18,
+      description: "asdk;AVJBSERHV[S EOIRPAIEORGNHAIOUERHV;sdvhapufbvaerhgfjpa ergviuaergvoyewugf",
+      comments: [
+        {
+          author: "dasd",
+          text: "asdasdsa",
+          date: "asdasd"
+        }
+      ]
+    }
+
+    let hulk:Movie = {
+      id: 1,
+      title: "Hulk",
+      country: "aafad",
+      poster: "",
+      genre: "triller",
+      premiere:  "13123",
+      duration: "1212",
+      age: 18,
+      description: "asdk;AVJBSERHV[S EOIRPAIEORGNHAIOUERHV;sdvhapufbvaerhgfjpa ergviuaergvoyewugf",
+      comments: [
+        {
+          author: "dasd",
+          text: "asdasdsa",
+          date: "asdasd"
+        }
+      ]
+    }
+    this.movies.push(avenger)
+    this.movies.push(hulk)
   }
 
 }
