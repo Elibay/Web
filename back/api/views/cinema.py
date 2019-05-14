45b6889ad4e163bbc3169fcf13282ab5eeb5db6c
Nurymzhan <nayapbergen@gmail.com>
2019-05-15 01:02:24 +0600

    ADD: views for cinema

--- back/api/views/cinema.py
+++ back/api/views/cinema.py
@@ -1,8 +1,14 @@
+from django.utils.decorators import method_decorator
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from api.serializers import CinemaSerializer
+from api.models import Cinema
+from api.utils.decorators import response_wrapper
 
 
 @method_decorator(response_wrapper(), name='dispatch')
-class NewsViewSet(viewsets.ModelViewSet):
-    queryset = News.objects.all()
+class CinemaViewSet(viewsets.ModelViewSet):
+    queryset = Cinema.objects.all()
     permission_classes = (AllowAny,)
     http_method_names = ['get']
-    serializer_class = NewsSerializer
+    serializer_class = CinemaSerializer
